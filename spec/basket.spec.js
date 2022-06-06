94dc93c4411ee932b748502f6207ecbdfc97d8d0
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-06-06 17:39:32 +0100

    adding file with extension 1 additions

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -7,7 +7,8 @@ describe("Basket", () => {
     const expected = [{
       item: "cream cheese bagel",
       price: 1.20,
-      quantity: 1
+      quantity: 1,
+      quantityForSum: 1
     }]
     // execute
     const result = basket.addToBasket("cream cheese bagel", 1.20, 1)
@@ -23,7 +24,8 @@ describe("Basket", () => {
     const expected = [{
       item: "cream cheese bagel",
       price: 1.20,
-      quantity: 1
+      quantity: 1,
+      quantityForSum: 1
     }]
     // execute
     const result = basket.removeFromBasket("smoked salmon bagel", 1)
@@ -62,15 +64,18 @@ describe("Basket", () => {
     const expected = [{
         item: "cream cheese bagel",
         price: 1.20,
-        quantity: 3
+        quantity: 3,
+        quantityForSum: 3
       }, {
         item: "smoked salmon bagel",
         price: 1.80,
-        quantity: 3  
+        quantity: 3,
+        quantityForSum: 3
       }, {
         item: "bacon bagel",
         price: 1.60,
-        quantity: 1    
+        quantity: 1,
+        quantityForSum: 1    
       }]
     // execute
     const result = basket.addToBasket("bacon bagel", 1.60, 1)
@@ -110,7 +115,8 @@ describe("Basket", () => {
     const expected = [{
       item: "cream cheese bagel",
       price: 1.20,
-      quantity: 2
+      quantity: 2,
+      quantityForSum: 2
     }]
     // execute
     const result = basket.addToItemQuantity("cream cheese bagel", 1)
@@ -124,6 +130,34 @@ describe("Basket", () => {
     basket.addToBasket("cream cheese bagel", 1.20, 1)
     basket.addToBasket("smoked salmon bagel", 1.80, 2)
     const expected = 4.80
+    // execute
+    const result = basket.checkoutTotalSum()
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("creates a large order utilising special offers, provides checkout sum", () => {
+    // set up
+    const basket = new Basket()
+    basket.increaseBasketCapacity(20)
+    basket.addToBasket("onion bagel", 0.49, 2)
+    basket.addToBasket("coffee", 0.99, 3)
+    basket.plainSpecial(1)
+    basket.everythingSpecial(1)
+    const expected = 10.43
+    // execute
+    const result = basket.checkoutTotalSum()
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("creates a different large order utilising special offers, provides checkout sum", () => {
+    // set up
+    const basket = new Basket()
+    basket.increaseBasketCapacity(20)
+    basket.addToBasket("plain bagel", 0.39, 4)
+    basket.plainSpecial(1)
+    const expected = 5.55
     // execute
     const result = basket.checkoutTotalSum()
     // verify
--- src/basket.js
+++ src/basket.js
@@ -1,3 +1,5 @@
+const Inventory = require('../inventory.json').inventory
+
 class Basket {
     constructor () {
         this.items = []
@@ -11,7 +13,8 @@ class Basket {
         const basketItem = {
             item: itemName,
             price: itemPrice,
-            quantity: itemQuantity
+            quantity: itemQuantity,
+            quantityForSum: itemQuantity
         }
 
         let basketQuantity = 0
@@ -69,6 +72,7 @@ class Basket {
         const itemToIncreaseQuantity = this.items.find(item => item.item === itemName)
 
         itemToIncreaseQuantity.quantity += itemQuantity
+        itemToIncreaseQuantity.quantityForSum += itemQuantity
 
         if ((itemToIncreaseQuantity.quantity) > this.maxCapacity) {
 
@@ -85,11 +89,137 @@ class Basket {
 
         for(let i = 0; i < this.items.length; i++) {
 
-            totalSum += (this.items[i].quantity*this.items[i].price)
+            totalSum += (this.items[i].quantityForSum*this.items[i].price)
 
         }
 
-        return totalSum
+        const rounded = Math.round((totalSum + Number.EPSILON) * 100) / 100;
+
+        return rounded
+    }
+
+    onionSpecial(quantityOfSpecial) {
+
+        // need to add tests for undefined/null
+
+        const basketItem = {
+            item: "Deal: onion bagels, 6 for 2.49",
+            price: 2.49*quantityOfSpecial,
+            quantity: 6*quantityOfSpecial,
+            quantityForSum: quantityOfSpecial
+        }
+
+        let basketQuantity = 0
+        
+        for (let i = 0; i < this.items.length; i++) {
+
+             basketQuantity += this.items[i].quantity
+
+        }
+
+        if ((basketQuantity + basketItem.quantity) > this.maxCapacity) {
+
+            return "Your basket is full, this item has not been added to your basket"
+
+        }
+
+        this.items.push(basketItem)
+
+
+        return this.items
+    }
+
+    plainSpecial(quantityOfSpecial) {
+
+        // need to add tests for undefined/null
+
+        const basketItem = {
+            item: "Deal: plain bagels, 12 for 3.99",
+            price: 3.99*quantityOfSpecial,
+            quantity: 12*quantityOfSpecial,
+            quantityForSum: quantityOfSpecial
+        }
+
+        let basketQuantity = 0
+        
+        for (let i = 0; i < this.items.length; i++) {
+
+             basketQuantity += this.items[i].quantity
+
+        }
+
+        if ((basketQuantity + basketItem.quantity) > this.maxCapacity) {
+
+            return "Your basket is full, this item has not been added to your basket"
+
+        }
+
+        this.items.push(basketItem)
+
+
+        return this.items
+    }
+
+    everythingSpecial(quantityOfSpecial) {
+
+        // need to add tests for undefined/null
+
+        const basketItem = {
+            item: "Deal: everything bagels, 6 for 2.49",
+            price: 2.49*quantityOfSpecial,
+            quantity: 6*quantityOfSpecial,
+            quantityForSum: quantityOfSpecial
+        }
+
+        let basketQuantity = 0
+        
+        for (let i = 0; i < this.items.length; i++) {
+
+             basketQuantity += this.items[i].quantity
+
+        }
+
+        if ((basketQuantity + basketItem.quantity) > this.maxCapacity) {
+
+            return "Your basket is full, this item has not been added to your basket"
+
+        }
+
+        this.items.push(basketItem)
+
+
+        return this.items
+    }
+
+    coffeeAndPlainSpecial(quantityOfSpecial) {
+
+        // need to add tests for undefined/null
+
+        const basketItem = {
+            item: "Deal: coffee and a plain bagel for 1.25",
+            price: 1.25*quantityOfSpecial,
+            quantity: 2*quantityOfSpecial,
+            quantityForSum: quantityOfSpecial
+        }
+
+        let basketQuantity = 0
+        
+        for (let i = 0; i < this.items.length; i++) {
+
+             basketQuantity += this.items[i].quantity
+
+        }
+
+        if ((basketQuantity + basketItem.quantity) > this.maxCapacity) {
+
+            return "Your basket is full, this item has not been added to your basket"
+
+        }
+
+        this.items.push(basketItem)
+
+
+        return this.items
     }
 
 }
